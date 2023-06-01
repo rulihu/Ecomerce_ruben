505a266dcbb88b4a49400a4b90829e6c2be3d76b
rulihu <ruf.lima@gmail.com>
2023-05-31 21:47:12 -0400

    cambios

--- services/productos-servicios.js
+++ services/productos-servicios.js
@@ -1,7 +1,9 @@
-const url = "http://localhost:3000/productos"
-
+//const url = "http://localhost:3000/productos"
+const url = "https://my-json-server.typicode.com/rulihu/Api_Ecomerse/productos"
 /*Es para la parte de solo usuarios*/
-const url2 = "http://localhost:3000/usuarios"
+//const url2 = "http://localhost:3000/usuarios"
+const url2 = "https://my-json-server.typicode.com/rulihu/Api_Ecomerse/usuarios"
+
 async function listaUsuarios(){
     return await fetch(url2).then(respuesta => respuesta.json());
 }
@@ -19,19 +21,22 @@ async function crearProducto(nombre, precio, imagen, categoria, link) {
 }
 
 const eliminarProducto = (id) => {
-    return fetch(`http://localhost:3000/productos/${id}`, {
+    //return fetch(`http://localhost:3000/productos/${id}`, {
+    return fetch(url+`/${id}`, {
       method: "DELETE",
     });
   };
 
 const detalleProducto = (id) => {
-    return fetch(`http://localhost:3000/productos/${id}`).then((respuesta) =>
+    //return fetch(`http://localhost:3000/productos/${id}`).then((respuesta) =>
+    return fetch(url+`/${id}`).then((respuesta) =>
         respuesta.json()
     );
 };
 
 const actualizaProducto = (id,nombre,precio,imagen,categoria,link) => {
-    return fetch(`http://localhost:3000/productos/${id}`,{
+    //return fetch(`http://localhost:3000/productos/${id}`,{
+    return fetch(url+`/${id}`,{
         method: 'PUT',
         headers: {
             "Content-Type": "application/json",
